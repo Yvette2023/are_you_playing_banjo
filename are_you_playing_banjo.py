5431d286b1b17ef9c2a389bce1f588e0a36cec42
Ingabire Yvette <johndoe@example.com>
2023-01-16 22:44:47 +0100

    ici est mon commit

--- are_you_playing_banjo.py
+++ are_you_playing_banjo.py
@@ -3,6 +3,11 @@ def are_you_playing_banjo(name):
         return name + " plays banjo" 
     else:
         return name + " does not play banjo"
+
+    if name[0]=="Y" or name[0]=="s":
+        return name + " plays guitar" 
+    else:
+        return name + " does not play guitar"
     
 print(are_you_playing_banjo("pop"))
 
